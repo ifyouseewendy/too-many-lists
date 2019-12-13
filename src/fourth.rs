5b0a380afb4e21bf7d5d3e6f8d17ea8c746b48a3
Di Wen <ifyouseewendy@gmail.com>
2019-12-12 22:20:51 -0500

    5.4 Add peek_front with Ref

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 
 pub struct List<T> {
     head: Link<T>,
@@ -62,6 +62,12 @@ impl<T> List<T> {
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
     }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -93,5 +99,14 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn peek_front() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+
+        list.push_front(1); list.push_front(2); list.push_front(3);
+        assert_eq!(*list.peek_front().unwrap(), 3);
     }
 }
