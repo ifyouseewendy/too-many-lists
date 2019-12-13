b22341e57f2635f68414dcdc45e50391bdb5d52d
Di Wen <ifyouseewendy@gmail.com>
2019-12-13 10:03:44 -0500

    5.6 Add into_iter

--- src/fourth.rs
+++ src/fourth.rs
@@ -117,9 +117,32 @@ impl<T> List<T> {
     }
 }
 
+// Drop
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while self.pop_front().is_some() {}
+    }
+}
+
+// Iterators
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+// IntoIter
+pub struct IntoIter<T>(List<T>);
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.pop_back()
     }
 }
 
@@ -196,5 +219,25 @@ mod test {
         assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
         assert_eq!(&*list.peek_back().unwrap(), &1);
         assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+
+        let mut list = List::new();
+        list.push_front(1); list.push_front(2); list.push_front(3);
+        let mut rev = list.into_iter().rev();
+        assert_eq!(rev.next(), Some(1));
+        assert_eq!(rev.next(), Some(2));
+        assert_eq!(rev.next(), Some(3));
     }
 }
